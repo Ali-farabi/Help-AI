0e6e14f161db528bac226652783c27b51c5884d8
Ali <kultaiali670@gmail.com>
2025-12-12 03:04:17 +0500

    adding

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/', 
+  base: mode === 'production' ? '/Help-AI/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -19,4 +19,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
