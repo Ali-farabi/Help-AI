67261c09f8ebf3566d18669332a9f1ec1f5e4a92
Ali <kultaiali670@gmail.com>
2025-12-11 18:56:33 +0500

    dsadas

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Help-AI/', 
+  base: '/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
